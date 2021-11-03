323a4045a817a85a0f385c156bf3fca09d3ada14
Darren Henderson <darrenhusa@yahoo.com>
2021-11-03 10:19:30 -0500

    Added vue3.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+import { createApp } from 'vue'
+
+import HelloWorld from './components/HelloWorld.vue';
+
+const app = createApp({});
+
+// registers our HelloWorld component globally
+app.component('hello-world', HelloWorld);
+
+// mount the app to the DOM
+app.mount('#app');
